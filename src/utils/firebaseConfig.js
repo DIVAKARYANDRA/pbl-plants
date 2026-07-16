11643b862bf6031dd59e07c8f3fa4e708af92367
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-07-16 21:34:32 +0530

    Create firebaseConfig.js

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAbciGk_YcZ4Oir-0pndDdnlspFT8TzX4",
+  authDomain: "pbl-plants.firebaseapp.com",
+  projectId: "pbl-plants",
+  storageBucket: "pbl-plants.firebasestorage.app",
+  messagingSenderId: "301985133309",
+  appId: "1:301985133309:web:8e7b6956534dbd936b8494"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
