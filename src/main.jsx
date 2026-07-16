896a57485748bbe4d3c0ceaa25cd3923b3c0ebee
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-07-16 21:47:47 +0530

    Update main.jsx (restoring back to original)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { testFirebaseConnection } from "./utils/firebaseTest";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -10,4 +9,3 @@ createRoot(document.getElementById("root")).render(
   </StrictMode>
 );
 
-testFirebaseConnection();
