5448d888bb8b56ca8221d24dc67deb13ef4f533d
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-07-16 22:21:23 +0530

    error fixing with firebase

--- src/context/SiteDataContext.jsx
+++ src/context/SiteDataContext.jsx
@@ -11,6 +11,7 @@ import {
 } from "firebase/firestore";
 
 import { db } from "../utils/firebaseConfig";
+console.log("Firebase DB object:", db);
 
 const STORAGE_KEY = "pbl-plants:database:v1";
 const SiteDataContext = createContext(null);
