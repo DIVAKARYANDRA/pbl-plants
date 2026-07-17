5b065264e6fa2b2e153be50da1942aa271c69ea5
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-07-17 09:57:46 +0530

    Update ProtectedRoute.jsx

--- src/admin/ProtectedRoute.jsx
+++ src/admin/ProtectedRoute.jsx
@@ -2,7 +2,8 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 export default function ProtectedRoute() {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+  if (loading) return null;
   if (!isAuthenticated) return <Navigate to="/admin/login" replace />;
   return <Outlet />;
 }
