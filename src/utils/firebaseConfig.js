8b5d8ffdadf52c552e068811a11cbfeb5e1bc7be
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-07-17 09:33:02 +0530

    Update firebaseConfig.js (added authentication from firebase)

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAAbciGk_YcZ4Oir-0pndDdnlspFT8TzX4",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
