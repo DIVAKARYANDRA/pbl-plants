66e8765ea239a94f4250e0efc1cae826091595e9
YANDRA DIVAKAR <99150919+DIVAKARYANDRA@users.noreply.github.com>
2026-07-27 14:13:00 +0530

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {
+
+    console.log(
+      "New update available"
+    );
+
+  },
+
+  onOfflineReady() {
+
+    console.log(
+      "App ready offline"
+    );
+
+  },
+
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,69 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+
+  plugins:[
+
+    react(),
+
+    VitePWA({
+
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "pbl-logo.jpg"
+      ],
+
+
+      manifest: {
+
+        name: "PBL Plants Admin",
+
+        short_name: "PBL Admin",
+
+        description:
+          "PBL Plants Management Dashboard",
+
+
+        theme_color:
+          "#245c3a",
+
+
+        background_color:
+          "#ffffff",
+
+
+        display:
+          "standalone",
+
+
+        start_url:
+          "/admin",
+
+
+        icons:[
+
+          {
+            src:"/pbl-logo.jpg",
+            sizes:"192x192",
+            type:"image/jpg"
+          },
+
+          {
+            src:"/pbl-logo.jpg",
+            sizes:"512x512",
+            type:"image/jpg"
+          }
+
+        ]
+
+      }
+
+    })
+
+  ]
+
 })
